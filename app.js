c0fa50fd533de41c9571956f09ef28745272d1cb
Sabina Iurascu <sabina_iurascu@hotmail.co.uk>
2021-07-27 09:59:04 +0200

    Added line animation for navigation

--- app.js
+++ app.js
@@ -1,86 +1,48 @@
-window.addEventListener("load", function () {
-  let allCards = document.querySelectorAll(".card");
+setTimeout(function () {
+  console.log("script loaded");
+  $("body").on("click", ".next", function () {
+    var id = $(".content:visible").data("id");
+    var nextId = $(".content:visible").data("id") + 1;
+    $('[data-id="' + id + '"]').hide();
+    $('[data-id="' + nextId + '"]').show();
+    document.querySelector(".btn__text-dynamic").classList.add("test1");
 
-  // retrieve first section and expand it
-  let firstSection = allCards[0].querySelector(".collapse");
-  firstSection.classList.add("show");
-  allCards[0].classList.add("bs-callout-custom");
+    console.log($(".back:hidden").length);
+    if ($(".back:hidden").length == 1) {
+      console.log("Show button");
+      $(".back").show();
+    }
 
-  // remove "next" button from the last section
-  let lastSection = allCards[allCards.length - 1]
-    .querySelector(".btn-next")
-    .remove();
-
-  let allExpandTriggers = document.querySelectorAll(".collapsed");
-
-  allExpandTriggers.forEach((item) => {
-    item.addEventListener("click", function () {
-      setTimeout(function () {
-        let currentSection = item.closest(".card").querySelector(".collapse");
-        if (currentSection.classList.contains("show")) {
-          toggleSuccess();
-        }
-      }, 500);
-    });
+    if (nextId == 4) {
+      $(".content-holder").hide();
+      $(".end").show();
+    }
   });
-});
 
-function expandNextSection(e) {
-  let currentCard = e.closest(".card");
-  let nextCard = currentCard.nextElementSibling;
+  $("body").on("click", ".back", function () {
+    document.querySelector(".btn__text-dynamic").classList.remove("test1");
 
-  currentCard.classList.add("bs-callout-success");
-  nextCard.classList.add("bs-callout-custom");
+    var id = $(".content:visible").data("id");
+    var prevId = $(".content:visible").data("id") - 1;
+    $('[data-id="' + id + '"]').hide();
+    $('[data-id="' + prevId + '"]').show();
 
-  let nextCardCollapsibleSection = nextCard.querySelector(".collapse").id;
-  //let nextSectionID = "#" + nextCardCollapsibleSection;
-
-  //   e.setAttribute("data-toggle", "collapse");
-  //   e.setAttribute("data-target", nextSectionID);
-  //   e.setAttribute("data-expanded", "false");
-  e.setAttribute("data-controls", "nextCardCollapsibleSection");
-
-  // let nextCard = currentCard.nextElementSibling;
-
-  // if (nextCard !== null && !nextCard.classList.contains("bs-callout-success")) {
-  //   nextCard.classList.add("bs-callout-custom");
-  // }
-}
-
-function changeColour(e) {
-  var currentlyActive = document.querySelector(".bs-callout-custom");
-
-  if (currentlyActive !== null) {
-    currentlyActive.classList.remove("bs-callout-custom");
-    currentlyActive.classList.add("bs-callout-success");
-  }
-
-  let currentCard = e.closest(".card");
-  currentCard.style.background = "yellow";
-
-  if (
-    !currentCard.classList.contains("bs-callout-custom") &&
-    !currentCard.classList.contains("bs-callout-success")
-  ) {
-    currentCard.classList.add("bs-callout-custom");
-  } else {
-    currentCard.classList.remove("bs-callout-custom");
-    currentCard.classList.add("bs-callout-success");
-  }
-}
-
-function toggleSuccess() {
-  var inputFields = document.querySelectorAll(".input-field");
-
-  inputFields.forEach((item) => {
-    item.addEventListener("focus", () => {
-      var allActiveDivs = document.querySelectorAll(".bs-callout-custom");
-      allActiveDivs.forEach((item) => {
-        item.classList.remove("bs-callout-custom");
-      });
-
-      item.closest(".card").classList.remove("bs-callout-success");
-      item.closest(".card").classList.add("bs-callout-custom");
-    });
+    if (prevId == 1) {
+      console.log("Hide button");
+      $(".back").hide();
+    }
   });
-}
+
+  $("body").on("click", ".edit-previous", function () {
+    $(".end").hide();
+    $(".content-holder").show();
+    $("#content-3").show();
+  });
+
+  // var button = document.querySelector(".btn__text-dynamic");
+
+  // button.addEventListener("click", function (e) {
+  //   console.log(e.target);
+  //   document.querySelector(".btn__text-dynamic").classList.add("test1");
+  // });
+}, 2000);
